b1c9b05b6156565dc10761e84a6db28441f0f797
Peake <kaylee.peake@uleth.ca>
2020-02-13 14:52:29 -0700

    done

--- Anti-LifeCoach/www/js/index.js
+++ Anti-LifeCoach/www/js/index.js
@@ -1,5 +1,15 @@
 console.log("yo");
 
+var $$ = Dom7;
+
+var badDeed = $(".item-input-info")
+
+var counter = 0;
+
+var cSrc;
+
+var rants = ["I need clothes!", "Do bad things!", "Skip class to finish your app!", "ENERGY DRINKS"]
+
 var app = new Framework7({
     //app root element!
     root: '#app',
@@ -23,3 +33,39 @@ var app = new Framework7({
 });
 
 var mainView = app.views.create('.view-main');
+
+
+
+    setInterval(function(){
+        
+    $(".rants").text(rants[counter]);
+        counter++;
+        
+        if(counter == rants.length){
+            
+            counter = 0;
+        }
+        
+    }, 4000);
+
+
+$(".devil").on("click", function(){
+    
+   cSrc = $(this).attr("src");
+});
+
+$$(document).on('page:init', '.page[data-name="page2"]', function() {
+    
+    console.log(cSrc);
+    
+    $("#devil").html("<img src='" + cSrc + "' class='pet'>");
+    
+    $("#naked_devil").remove()
+    
+});
+
+$(".button").on("click", function(){
+    
+    console.log("purchase");
+    $("#naked_devil").html('<img src="img/gifs/ballerina_blink.gif">');
+});
